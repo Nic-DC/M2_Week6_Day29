cbbd95dabf15f304e79fa3478057e81c3a1567b6
Nic <nic.dan.costea@gmail.com>
2022-11-04 16:24:52 +0100

    checkpoint

--- app/script.js
+++ app/script.js
@@ -1,17 +1,56 @@
-console.log("olaaa");
+console.log("olaa");
 
-// Extra 9 - hide the card details when clicking image
-const image = document.getElementById("ex9");
-const details = document.getElementById("ex9Details");
-details.classList.add("hideDetails");
-const hideCardDetails = function () {
-  details.classList.toggle("hideDetails");
+// Ex 3
+
+const favorites = document.querySelectorAll("#responsive .card");
+const genre = function () {
+  for (let fav of favorites) {
+    const genreBadge = document.createElement("span");
+    genreBadge.classList.add("badge");
+    genreBadge.classList.add("badge-warning");
+    genreBadge.classList.add("badgeStyling");
+    genreBadge.innerText = "Soul-Rock";
+    fav.appendChild(genreBadge);
+  }
 };
-image.addEventListener("click", hideCardDetails);
+genre();
 
-// Extra 10 - done in html file
+// Ex 5
+const dangers = document.querySelectorAll("#tableAlbums .btn-danger");
+const deleteSong = function () {
+  for (let danger of dangers) {
+    danger.addEventListener("click", () => {
+      console.log("danger");
+      danger.parentElement.parentElement.remove(0);
+    });
+  }
+};
+deleteSong();
 
-// Extra 11
-const allAlbums = document.getElementById("albumsSection");
-const totalAlbums = document.getElementById("totalAlbums");
-totalAlbums.innerText = `${allAlbums.children.length}`;
+const tableBody = document.getElementById("tableBody");
+// Ex 8
+const addNewTrack = document.getElementById("addNewTrack");
+const trackAdded = function () {
+  const number = document.getElementById("number");
+  const numberValue = number.value;
+  const artist = document.getElementById("artist");
+  const artistValue = artist.value;
+  const title = document.getElementById("title");
+  const titleValue = title.value;
+  const duration = document.getElementById("duration");
+  const durationValue = duration.value;
+
+  if (number.value !== "" && number.value !== "" && number.value !== "" && number.value !== "") {
+    tableBody.innerHTML += `
+    <tr>
+      <th scope="row">${numberValue}</th>
+      <td>${titleValue}</td>
+      <td>${artistValue}></td>
+      <td>${durationValue}</td>
+      <td><button type="button" class="btn btn-danger">Delete</button></td>
+    </tr>
+  `;
+  }
+};
+addNewTrack.addEventListener("click", trackAdded);
+// number, artist, title, duration
